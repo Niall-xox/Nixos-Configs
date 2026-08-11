c45982019cb1a41dae4faa002e838392b658a1e0
niall <niall.fatah20032gmail.com>
2026-08-11 21:06:06 +0100

    final fix for using lg tv with settup, hl.output uses monitor descriptor now rather than hdmi label

--- hosts/nialls-pc/monitors.lua
+++ hosts/nialls-pc/monitors.lua
@@ -1,21 +1,10 @@
 hl.monitor({
-  output = "HDMI-A-1",
+  output = "desc:LG Electronics LG TV SSCR2 0x01010101",
   mode = "3840x2160@120",
   position = "0x0",
   scale = 1.5,
   bitdepth = 10,
-  cm = "auto",	
-  vrr = 1,
-  supports_hdr = 1,
-})
-
-hl.monitor({
-  output = "HDMI-A-2",
-  mode = "3840x2160@120",
-  position = "0x0",
-  scale = 1.5,
-  bitdepth = 10,
-  cm = "auto",	
+  cm = "auto",
   vrr = 1,
   supports_hdr = 1,
 })
