878c569b6d8348cf855cdb33794bdb67fe791c4d
niall <niall.fatah20032gmail.com>
2026-08-11 20:51:27 +0100

    niallspc monitors.lua edit for hdmia1 and a2 bug

--- hosts/nialls-pc/monitors.lua
+++ hosts/nialls-pc/monitors.lua
@@ -5,6 +5,17 @@ hl.monitor({
   scale = 1.5,
   bitdepth = 10,
   cm = "auto",	
-  vrr = 0,
+  vrr = 1,
+  supports_hdr = 1,
+})
+
+hl.monitor({
+  output = "HDMI-A-2",
+  mode = "3840x2160@120",
+  position = "0x0",
+  scale = 1.5,
+  bitdepth = 10,
+  cm = "auto",	
+  vrr = 1,
   supports_hdr = 1,
 })
